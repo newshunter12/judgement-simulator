a44d3705e3423d3bd865257e221b092ff4deccb8
kimHako <l36h7129@gmail.com>
2020-10-03 15:27:14 +0900

    feat: create Description component
    - added svg icon
    - used state for handle contents order

--- component/description.tsx
+++ component/description.tsx
@@ -1,11 +1,41 @@
 import styles from "./Description.module.scss";
-import React from "react";
+import PrevIcon from "public/imgs/arrow-left.svg";
+import NextIcon from "public/imgs/arrow-right.svg";
+import React, { useState } from "react";
 
-export default function Description(caseTitle, content): React.ReactElement {
+interface Props {
+  instructions: {
+    content: string;
+  }[];
+  caseTitle: string;
+}
+
+export default function Description({ instructions, caseTitle }: Props): React.ReactElement {
+  const [index, setIndex] = useState(0);
+
+  const title = caseTitle;
+  const content = instructions[index].content;
+  const img = `IMAGE_${index}`; //TODO: 이미지명 caseNum_n 으로 저장
+
   return (
     <div>
-      <div className={styles.title}>{caseTitle}</div>
-      <div className={styles.content}>{content}</div>
+      <div>
+        <PrevIcon
+          className={index === 0 ? styles.inActive : ""}
+          onClick={() => setIndex(index - 1)}
+        />
+      </div>
+      <div className={styles.title}> {title}</div>
+      <div>
+        <div className={styles.content}>{content}</div>
+        <div> {img}</div>
+      </div>
+      <div>
+        <NextIcon
+          className={index === 2 ? styles.inActive : ""}
+          onClick={() => setIndex(index + 1)}
+        />
+      </div>
     </div>
   );
 }
