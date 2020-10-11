e7625519dcd54de53116d41d318b23b5692ecf17
kimHako <l36h7129@gmail.com>
2020-10-11 15:00:37 +0900

    feat: create process tab
    - applied CSS to selected tab

--- components/Judgement.tsx
+++ components/Judgement.tsx
@@ -1,45 +1,70 @@
 import CasePsychology from "./CasePsychology";
 import Description from "./Description";
+import styles from "./Judgement.module.scss";
 import ResultReport from "./ResultReport";
 import SentenceCondition from "./SentenceCondition";
 import Sentencing from "./Setencing";
+import NextArrowIcon from "public/imgs/arrow-right.svg";
 import Case from "utils/Case.interface";
-import React from "react";
+import React, { useState, useRef } from "react";
 
 interface Props {
   caseName: string;
   caseObject: Case | undefined;
 }
 
-//TODO: 유저가 어디 메뉴에 있는지에 대한 정보를 useState로 두기
 function Judgement({ caseObject, caseName }: Props): React.ReactElement {
   if (caseObject === undefined) return <div>Loading...</div>;
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const menuList = useRef<HTMLUListElement>(null);
+
+  const taps = ["사건 개요", "양형 조건", "사건 심리", "가중/감경", "당신의 판결"];
+
+  const comps = [
+    <div key={0}>
+      <h1>{caseName}</h1>
+      <Description
+        instructions={caseObject.instructions}
+        caseTitle={caseObject.caseTitle}
+        caseNum={caseObject.caseNum}
+      />
+    </div>,
+    <div key={1}>
+      <SentenceCondition statutes={caseObject.statutes} />
+    </div>,
+    <div key={2}>
+      <CasePsychology petition={caseObject.petition} />
+    </div>,
+    <div key={3}>
+      <Sentencing plusImpacts={caseObject.plusImpacts} minusImpacts={caseObject.minusImpacts} />
+    </div>,
+    <div key={4}>
+      <ResultReport result={caseObject.result} epilogue={caseObject.epilogue} />
+    </div>,
+  ];
+
+  function onChangeIndex(i: number) {
+    setSelectedIndex(i);
+    const nowProcess = menuList.current.children[i] as HTMLElement;
+    nowProcess.style.color = "red";
+  }
+
   return (
     <div>
+      <ul ref={menuList}>
+        {taps.map((tab, i) => (
+          <li key={i} className={styles.menu}>
+            {tab}
+          </li>
+        ))}
+      </ul>
+      {comps[selectedIndex]}
       <div>
-        <h1>{caseName}</h1>
-
-        <Description
-          instructions={caseObject.instructions}
-          caseTitle={caseObject.caseTitle}
-          caseNum={caseObject.caseNum}
-        />
-      </div>
-      <hr />
-      <div>
-        <SentenceCondition statutes={caseObject.statutes} />
-      </div>
-      <hr />
-      <div>
-        <CasePsychology petition={caseObject.petition} />
-      </div>
-      <hr />
-      <div>
-        <Sentencing plusImpacts={caseObject.plusImpacts} minusImpacts={caseObject.minusImpacts} />
-      </div>
-      <hr />
-      <div>
-        <ResultReport result={caseObject.result} epilogue={caseObject.epilogue} />
+        <NextArrowIcon
+          onClick={() => onChangeIndex(selectedIndex + 1)}
+          className={selectedIndex === comps.length - 1 ? styles.inActive : ""}
+        ></NextArrowIcon>
       </div>
     </div>
   );
