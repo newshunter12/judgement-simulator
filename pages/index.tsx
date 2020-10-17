a9a3444b3f8a44188a0db25c8e35d9bb7613b8fa
kimHako <l36h7129@gmail.com>
2020-10-17 18:19:34 +0900

    feat: add case

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,40 @@
 import styles from "./index.module.scss";
-import Head from "next/head";
 import Link from "next/link";
 import React from "react";
 
 export default function Home(): React.ReactElement {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Judgement Simulator</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <div className={styles.header}>판결문 시뮬레이터 </div>
-      <div className={styles.main}>
-        <Link href={`/data/소라넷사건`}>
-          <a>판결하러 가기</a>
-        </Link>
+    <>
+      <div className={styles.container}>
+        <head>
+          <title>Judgement Simulator</title>
+          <link rel="icon" href="/favicon.ico" />
+        </head>
+        <div className={styles.header}>판결문 시뮬레이터 </div>
+        <div className={styles.main}>
+          <Link href={`/data/온라인 성착취 사이트 운영자`}>
+            <a>온라인 성착취 사이트 운영자</a>
+          </Link>
+          <Link href={`/data/위력에 의한 성폭력`}>
+            <a>위력에 의한 성폭력</a>
+          </Link>
+          <Link href={`/data/온라인 성착취 사이트 운영자`}>
+            <a>온라인 성착취 사이트 운영자</a>
+          </Link>
+          <Link href={`/data/위력에 의한 성폭력`}>
+            <a>위력에 의한 성폭력</a>
+          </Link>
+        </div>
+        <footer className={styles.footer}>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ITPAS
+          </a>
+        </footer>
       </div>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ITPAS
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
